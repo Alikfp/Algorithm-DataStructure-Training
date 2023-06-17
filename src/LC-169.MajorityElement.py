9e08fec50de2dad5671ee6221537536e10ee8879
Alireza Kafaei <kafaeialireza@gmail.com>
2023-06-17 19:00:58 +1000

    Optimised LC-169

--- src/LC-169.MajorityElement.py
+++ src/LC-169.MajorityElement.py
@@ -1,3 +1,20 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         return sorted(nums)[len(nums)//2]
+    
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        score = 0
+        nom = nums[0]
+        for i in range(1, len(nums)):
+            #print('score', score)
+            #print('nom', nom)
+            if nums[i] == nom:
+                score += 1
+            elif (score>0) and (nums[i] != nom):
+                score -= 1
+            else:
+                score = 0
+                nom = nums[i]
+        return nom
+                
