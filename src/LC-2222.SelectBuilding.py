0259b898cc9beeca89019542ad1bf5ec7f9cfdf5
alirezakafaei <kafaeialireza@gmail.com>
2023-08-08 21:54:06 +1000

    Amazon prep Qs

--- src/LC-2222.SelectBuilding.py
+++ src/LC-2222.SelectBuilding.py
@@ -1,5 +1,24 @@
 class Solution:
     def numberOfWays(self, s: str) -> int:
+        memo = {
+            "0":0, "1":0, "01":0, "10":0, "101":0, "010":0
+        }
+        for num in s:
+            if num == "0":
+                memo["0"] += 1
+                memo["10"] += memo["1"]
+                memo["010"] += memo["01"]
+            elif num == "1":
+                memo["1"] += 1
+                memo["01"] += memo["0"]
+                memo["101"] += memo["10"]
+        return memo["101"] + memo["010"]
+        
+        
+        
+        
+        
+        """
         hsh_mp = {0:[i for i in range(len(s)) if s[i] == '0'],
                   1:[j for j in range(len(s)) if s[j] == '1']}
         #print(hsh_mp)
@@ -10,3 +29,4 @@ class Solution:
                     res += ((hsh_mp[x][i] - hsh_mp[x][i-1])- 1) * i * (n - i)
 
         return res
+        """
